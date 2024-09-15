c21c1bf978284cb955293b87d229045ca2d7b329
Lahiru Nanayakkara <lahirunanayakkara924@gmail.com>
2024-09-15 20:00:57 +0530

    Basic routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Header from "./components/Header";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
 const App = () => {
   return (
-    <div className="text-3xl text-red-700">Hello World</div>
-  )
-}
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<Navigate to={"/"} />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
